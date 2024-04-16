0cd58292c6879f0a070c4fa539d11cb828e62cd3
22gorfel <22gorfel@soltorgsgymnasiet.se>
2024-04-16 11:46:36 +0200

    New movement, textures.
    Added a texture pack, added textures on objects, added vaulting, Sens changer in inspection.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     //Rotation and look
     private float xRotation;
     private float sensitivity = 50f;
-    private float sensMultiplier = 1f;
+    [SerializeField] private float sensMultiplier = 1f;
 
     //Movement
     public float moveSpeed = 4500;
